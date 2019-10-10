67c08d09273641dee4cfd698918c16ad2e64cfb0
MahfuzKhandaker <mahfuzkhandaker.bd@gmail.com>
2019-10-10 13:23:06 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="jumbotron">
+          <div class="container">
+            <span class="display-4 h1">Vue.js Blog</span>
+            <p class="lead">Simple example how to connect to the server, paginate results, create single page app...</p>
+          </div>
+        </div>
+     </div>
+
+     <router-view></router-view>
+
+     <footer class="col-sm-12">
+       <hr>
+       <p class="text-center"><a href="http://www.codepress.com" target="_blank">codepress </a> &copy; 2019 </p>
+     </footer>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  max-width: 700px;
+}
+h1:first-letter, h2:first-letter {
+  text-transform:uppercase;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
+import VuePaginate from 'vue-paginate'
+import VueRouter from 'vue-router'
+import Blog from './Blog.vue'
+import PostDetail from './PostDetail.vue'
+
+Vue.use(VueResource);
+Vue.use(VuePaginate);
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
+// ENABLE SINGLE PAGE APP ROUTING
+const routes = [
+  { path: '/', component: Blog },
+  { path: '/post/:id', component: PostDetail }
+];
+
+const router = new VueRouter({
+  mode:'history',
+  routes
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
